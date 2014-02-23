b79b4c4ed657eb68aa6cef721be57e82882fffcc
sasaki <sasaki@tagbangers.co.jp>
2014-02-23 20:07:32 +0900

    Bugfix

--- src/main/java/org/wallride/web/controller/admin/page/PageDeleteController.java
+++ src/main/java/org/wallride/web/controller/admin/page/PageDeleteController.java
@@ -3,46 +3,53 @@ package org.wallride.web.controller.admin.page;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.wallride.core.domain.Page;
 import org.wallride.core.service.PageService;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
-import javax.validation.ValidationException;
 
 @Controller
-@RequestMapping(value="/pages/delete", method=RequestMethod.POST)
+@RequestMapping(value="/{language}/pages/delete", method=RequestMethod.POST)
 public class PageDeleteController {
-	
-	private static Logger logger = LoggerFactory.getLogger(PageDeleteController.class); 
-	
+
+	private static Logger logger = LoggerFactory.getLogger(PageDeleteController.class);
+
 	@Inject
 	private PageService pageService;
-	
+
 	@RequestMapping
-	public String delete(@Valid @ModelAttribute("form") PageDeleteForm form, BindingResult errors) {
-		if (!form.isConfirmed()) {
-			errors.rejectValue("confirmed", "Confirmed");
-		}
+	public String delete(
+			@Valid @ModelAttribute("form") PageDeleteForm form,
+			BindingResult errors,
+			RedirectAttributes redirectAttributes) {
+//		if (!form.isConfirmed()) {
+//			errors.rejectValue("confirmed", "Confirmed");
+//		}
 		if (errors.hasErrors()) {
 			logger.debug("Errors: {}", errors);
-			return "/page/delete";
+			return "/page/describe";
 		}
-		
+
+		Page page = null;
 		try {
-			pageService.deletePage(form.buildPageDeleteRequest(), errors);
+			page = pageService.deletePage(form.buildPageDeleteRequest(), errors);
 		}
-		catch (ValidationException e) {
+		catch (BindException e) {
 			if (errors.hasErrors()) {
 				logger.debug("Errors: {}", errors);
-				return "/page/delete";
+				return "/page/describe";
 			}
-			throw e;
+			throw new RuntimeException(e);
 		}
-		
-		return "/page/delete";
+
+		redirectAttributes.addFlashAttribute("deletedPage", page);
+		return "redirect:/_admin/{language}/pages/index";
 	}
 }
--- src/main/java/org/wallride/web/controller/admin/page/PageDescribeController.java
+++ src/main/java/org/wallride/web/controller/admin/page/PageDescribeController.java
@@ -3,6 +3,7 @@ package org.wallride.web.controller.admin.page;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,6 +28,15 @@ public class PageDescribeController extends DomainObjectDescribeController<Page,
 			Model model,
 			HttpSession session) {
 		return super.requestMappingDescribe(id, token, model, session);
+	}
+
+	@RequestMapping(params="part=delete-dialog")
+	public String partDeleteDialog(
+			@PathVariable String language,
+			@RequestParam long id, Model model) {
+		Page page = pageService.readPageById(id, language);
+		model.addAttribute("page", page);
+		return "/page/describe::#delete-dialog";
 	}
 
 	@Override
